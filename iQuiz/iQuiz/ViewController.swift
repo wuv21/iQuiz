aa4de063060efbda6d44c8b9024723747346fa23
studentuser <studentuser@D-69-91-165-117.dhcp4.washington.edu>
2016-05-04 13:43:11 -0700

    added in part1 code

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by studentuser on 5/4/16.
+//  Copyright © 2016 Vincent Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    @IBOutlet var subjectsTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        subjectsTable.dataSource = self
+        subjectsTable.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count;
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let reuseIdentifier = "subjectCell"
+        
+        var cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier) as UITableViewCell?
+        if (cell != nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+        }
+        
+        cell!.textLabel?.text = subjects[indexPath.row]
+        cell!.detailTextLabel?.text = subjects[indexPath.row]
+        cell!.imageView?.image = UIImage(named: "subject_pic.png")
+        
+        return cell!
+    }
+    
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func settingsClick(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here.", preferredStyle:UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+}
+
